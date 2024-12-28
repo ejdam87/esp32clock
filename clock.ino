a7fe50ba8e2579cea1247bc0ce5a3d64598cfe88
ejdam87 <adam.dzadon@gmail.com>
2024-12-28 19:54:11 +0100

    Fuse display control with servo

--- clock.ino
+++ clock.ino
@@ -4,7 +4,9 @@
 #include "WiFi.h"
 #include <time.h>
 #include <SPI.h>
+#include <ESP32Servo.h>
 
+// --- LED display
 // Pin definitions for MAX7219
 const int DIN_PIN = 23;  // Data In
 const int CS_PIN = 5;   // Chip Select
@@ -14,12 +16,28 @@ const int WIDTH = 8 * 4;
 const int HEIGHT = 8;
 const int DIGIT_WIDTH = 4;
 
+int prev_minute = -1;
+int prev_display_update = 0;
+int REFRESH_INTERVAL = 1000;
+// ---
+
+// --- Servo
+const int INITIAL_INVERT = -2;
+const int SERVO_OUT = 22;
+const int INVERT_INTERVAL = 180 * 1000;
+int prev_servo_update = -1;
+int direction = 0;
+// ---
+
+// --- Time synchronization
 const char* TIMEZONE = "CET-1CEST,M3.5.0/02:00:00,M10.5.0/03:00:00";
 const char* NTP_SERVER = "pool.ntp.org";
+// ---
 
 // Initialize the LedControl library
 // Parameters: DataIn, Clock, Load(CS), Number of MAX7219 devices
 ExtendedDisplay display = ExtendedDisplay(DIN_PIN, CLK_PIN, CS_PIN, 4);
+Servo servo;
 
 void putDigit( int digit, int segment )
 {
@@ -46,6 +64,17 @@ void showTime( struct tm* time_info )
   putDigit( hours % 10, 1 );
   hours /= 10;
   putDigit( hours % 10, 0 );
+
+  if ( prev_minute == -1 )
+  {
+    prev_minute = minutes % 10;
+  }
+
+  if ( minutes % 10 != prev_minute )
+  {
+    prev_servo_update = INITIAL_INVERT;
+    REFRESH_INTERVAL *= 60; // from the initial update enough to update after one minute
+  }
 
   putDigit( minutes % 10, 3 );
   minutes /= 10;
@@ -77,15 +106,37 @@ void setup()
   WiFi.disconnect();
   WiFi.mode( WIFI_OFF );
 
+  servo.attach(SERVO_OUT, 500, 2500);
+  servo.write( direction );
+}
+
+void handleDisplay()
+{
+  int t = millis();
+  if ( t - prev_display_update >= REFRESH_INTERVAL )
+  {
+    prev_display_update = t;
+    struct tm time_info = { 0 };
+    time_t now = { 0 };
+    time( &now );
+    localtime_r( &now, &time_info );
+    showTime( &time_info );
+  }
+}
+
+void handleServo()
+{
+  int t = millis();
+  if ( prev_servo_update == INITIAL_INVERT || t - prev_servo_update >= INVERT_INTERVAL )
+  {
+    prev_servo_update = t;
+    direction = ( direction + 180 ) % 360;
+    servo.write( direction );
+  }
 }
 
 void loop()
 {
-  struct tm time_info = { 0 };
-  time_t now = { 0 };
-  time( &now );
-  localtime_r( &now, &time_info );
-  showTime( &time_info );
-
-  delay( 1000 );
+  handleDisplay();
+  handleServo();
 }
